fb3f6c7f08150b49819278a4ee0f854cd9f6dc2c
Neokyuubi <neokyuubi@gmail.com>
2023-02-23 00:59:04 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { Canvas } from '@react-three/fiber';
+import "./style.css";
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+
+
+function CarShow()
+{
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3,2,5]} />
+
+      <mesh>
+        <boxGeometry args={[1,1,1]} />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
   );
 }
 
