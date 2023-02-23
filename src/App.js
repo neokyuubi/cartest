968aa109d4dbbc629d3b3cc3c5d1bb95504460bb
Neokyuubi <neokyuubi@gmail.com>
2023-02-23 18:05:26 +0100

    add rings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./style.css";
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Ground } from './Ground';
 import { Car } from './Car';
+import { Rings } from './Rings';
 
 
 function CarShow()
@@ -16,6 +17,7 @@ function CarShow()
       <color args={[0, 0, 0]} attach="background" />
 
       <Car />
+      <Rings/>
 
       <spotLight 
         color={[1, 0.25, 0.7]}
